744a97627449917835598c6db77f000c1aeff97a
felipegc <gfelipe@google.com>
2020-09-28 17:48:08 -0300

    refactoring the yaml manipulation to be easier to use

--- README.md
+++ README.md
@@ -20,13 +20,53 @@ Sample configuration file:
 
 ```yaml
 ---
-ip_regex: '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-ip_prefix: '1.1.1.'
-keywords:
-  'abc\.com': 'acme.com'
-  'www\.com': 'acme.org'
-  'zzz\..*\.com': 'acme.edu'
+global_regex:
+  'gr://machine_name': '[A-Z0-9-]+'
+  'gr://ipv4': '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+
+patterns:
+  - 'data': 'xml'
+    'field': 'MachineName'
+    'pattern': 'gr://machine_name'
+    'replace': 'machine_name'
+  - 'data': 'xml'
+    'field': 'IPAddress'
+    'pattern': 'gr://ipv4'
+    'replace': 'ipv4'
+  - 'data': 'raw'
+    'field': '-'
+    'pattern': 'gr://ipv4'
+    'replace': 'ipv4'
+  - 'data': 'raw'
+    'field': '-'
+    'pattern': 'NCR'
+    'replace': 'ALOHA'
 ```
+
+Detailes info about each configuratio file field:
+
+global_regex:
+
+You should define the regex and choose a proper name so that it can be reused by patterns. The sytax is:
+
+```
+  'gr://<name_of_pattern>': '<regex>'
+```
+
+patterns:
+
+```
+  - 'data': 'xml'
+    'field': 'Name'
+    'pattern': 'gr://<pattern_reference>'
+    'replace': '<replacer_function_name>'
+```
+
+You should define 4 fields here.
+  - data: It holds the type of data it is going to be looked for. The value might be: 'xml', 'kv', 'raw'.
+  - field: It holds the name of the field. Ex: if the 'data': 'xml' the field might be 'Name' so we have the full representation for that: \<Name>value\</Name>
+  - pattern: It holds the regex itself, a reference to a global_regex or a raw value.
+  - replace: It holds the function name used to replace the pattern found or a raw value it will be replaced by
 
 ## Install from local directory
 
--- sntzr/cli.py
+++ sntzr/cli.py
@@ -9,6 +9,7 @@ import yaml
 from sntzr.__version__ import __version__ as version
 
 global_patterns_values = {}
+all_config = None
 
 @click.command()
 @click.version_option(version=version)
@@ -16,11 +17,11 @@ global_patterns_values = {}
 @click.option('--config_file', default='./sntzr.yaml', help='Configuration file.')
 def main(input_file, config_file):
     """SNTZR"""
-    config = load_config(config_file)
+    global all_config
+    all_config = load_config(config_file)
     with open(input_file) as in_file:
         for line in in_file:
-            # TODO: felipegc we should call process line only if it is data
-            processed_line = process_line(line, config)
+            processed_line = process_line(line)
             print(processed_line, end='')
 
 
@@ -30,69 +31,54 @@ def load_config(config_file):
     return config
 
 
-def process_line(line, config):
-    # line = sanitize_ips(line, config['ip_regex'], config['ip_prefix'])
-    line = sanitize_global_patterns(line, config['global_patterns'])
-    line = sanitize_keywords(line, config['keywords'])
+def process_line(line):
+    line = sanitize_patterns(line)
     return line
 
 
-def sanitize_ips(line, ip_regex, ip_prefix):
-    ip_counter = 1
-    matches = re.findall(ip_regex, line)
-    if matches is not None:
-        unique_matches = set(matches)
-        for ip in unique_matches:
-            line = line.replace(ip, ip_prefix + str(ip_counter))
-            ip_counter = ip_counter + 1
-    return line
-
-
-def sanitize_keywords(line, keywords):
-    for pattern, repl in keywords.items():
-        line = re.sub(pattern, repl, line)
-    return line
-
-
+# Utils to manipulate global_pattern
 def build_full_pattern(global_pattern, value):
-    kindof = global_pattern['kindof']
-    kindof_placeholder =  global_pattern['kindof_placeholder']
+    """
+    Builds the pattern by merging the data type and field with the value dependig on the data.
+    Ex: if the data is xml and the field is Name the return will be <Name>value</Name>.
+    """
 
-    if kindof == 'raw':
+    data = global_pattern['data']
+    field =  global_pattern['field']
+
+    if data == 'raw':
         return value
-    elif kindof == 'tag_xml':
-        return '<{}>{}</{}>'.format(kindof_placeholder, value, kindof_placeholder)
-    elif kindof == 'kv':
-        return '{}={}'.format(kindof_placeholder, value)
+    elif data == 'xml':
+        return '<{}>{}</{}>'.format(field, value, field)
+    elif data == 'kv':
+        return '{}={}'.format(field, value)
 
 
-def sanitize_global_patterns(line, global_patterns):
-    for item in global_patterns:
-        regex = item['regex']
-        activated = item['activated']
+def extract_value(global_pattern, key):
+    """
+    Extracts the value from the key by removing tags, fields or something else it may have.
+    Ex: If the value is <Name>Jhon Dow</Name>, it will return 'Jonh Dow'.
+    """
 
-        if not activated:
-            continue
+    original_value = key
 
-        full_pattern_regex = build_full_pattern(item, regex)
+    if global_pattern['data'] == 'xml':
+        i_tag = '<{}>'.format(global_pattern['field'])
+        e_tag = '</{}>'.format(global_pattern['field'])
+        original_value = key.split(i_tag)[1].split(e_tag)[0]
+    elif global_pattern['data'] == 'kv':
+        kv = '{}='.format(global_pattern['field'])
+        original_value = key.split(kv)[1]
 
-        matches = re.findall(full_pattern_regex, line)
-
-        if len(matches) > 0:
-            unique_matches = set(matches)
-            for match in unique_matches:
-                # TODO: felipegc improve the way we are dealing with scapes. The way findall and sub deal with that is different
-                match = match.replace('\\', '\\\\')
-                if match in global_patterns_values:
-                    line = re.sub(match, global_patterns_values[match]['str_to_replace'], line)
-                else:
-                    add_global_pattern_value(item, match)
-                    line = re.sub(match, global_patterns_values[match]['str_to_replace'], line)
-
-    return line
+    return original_value
 
 
+# Utils to manipulate global_patterns_values
 def get_new_value_by_original_value(original_value):
+    """
+    Iterates over the patterns already saved in memory to find the new generated value for an original value.
+    """
+
     for item in global_patterns_values.values():
         if item['original_value'] == original_value:
             return item['new_value']
@@ -101,23 +87,18 @@ def get_new_value_by_original_value(original_value):
 
 
 def add_global_pattern_value(global_pattern, key):
-    original_value = key
+    """
+    Generates an object containing the original value, new value and the full pattern
+    to be replaced and saves it in memory to be re-used when necessary.
+    """
 
-    # TODO: felipegc create separate method for this
-    if global_pattern['kindof'] == 'tag_xml':
-        i_tag = '<{}>'.format(global_pattern['kindof_placeholder'])
-        e_tag = '</{}>'.format(global_pattern['kindof_placeholder'])
-        original_value = key.split(i_tag)[1].split(e_tag)[0]
-    elif global_pattern['kindof'] == 'kv':
-        kv = '{}='.format(global_pattern['kindof_placeholder'])
-        original_value = key.split(kv)[1]
-
+    original_value = extract_value(global_pattern, key)
 
     new_value = get_new_value_by_original_value(original_value)
 
     if new_value is None:
         length = len(original_value)
-        new_value = generate_random_value_by_kind(global_pattern['kind'], length)
+        new_value = generate_new_value(global_pattern, length)
 
     str_value_to_be_replaced = build_full_pattern(global_pattern, new_value)
 
@@ -128,75 +109,122 @@ def add_global_pattern_value(global_pattern, key):
     }
 
 
+def get_regex_pattern(pattern):
+    """
+    Tries to find the pattern in according to the key 'gr://' and returns its value otherwise
+    the pattern itself is returned.
+    """
+
+    global_regex = all_config['global_regex']
+
+    if pattern.startswith('gr://'):
+        return global_regex[pattern]
+    else:
+        return pattern
+
+
+def sanitize_patterns(line):
+    """
+    Iterates over the patterns in order to find them in the file and replace them by their custom value.
+    It preserves the found values in memory so it always replace the same patterns by the same values.
+    """
+
+    global_patterns = all_config['patterns']
+
+    for item in global_patterns:
+        regex = get_regex_pattern(item['pattern'])
+
+        full_pattern_regex = build_full_pattern(item, regex)
+
+        matches = re.findall(full_pattern_regex, line)
+
+        if len(matches) > 0:
+            unique_keys = set(matches)
+            for key in unique_keys:
+                # TODO: felipegc improve the way we are dealing with scapes. The way findall and sub deal with that is different
+                key = key.replace('\\', '\\\\')
+                if key in global_patterns_values:
+                    line = re.sub(key, global_patterns_values[key]['str_to_replace'], line)
+                else:
+                    add_global_pattern_value(item, key)
+                    line = re.sub(key, global_patterns_values[key]['str_to_replace'], line)
+
+    return line
+
+
+def generate_new_value(global_pattern, length=0):
+    """
+    Generates the value according to the replace function or value defined.
+    If the function is not found it uses the raw string istead.
+    """
+
+    replace = global_pattern['replace']
+
+    try:
+        return globals()[replace](length)
+    except:
+        return replace
+
+
+# Utils to generate random values
 def generate_random_hexdecimal(length, is_lower_case):
     random_value = binascii.hexlify(os.urandom(int(length/2)))
     str_value = random_value.decode('utf-8')
     return str_value if is_lower_case else str_value.lower()
 
 
-def generate_random_value_by_kind(kind, length=0):
-    if kind == 'hex_id' or kind == 'sha256':
-        return generate_random_hexdecimal(length, True)
+def mac_no_colon(length=0):
+    return generate_random_hexdecimal(length, True)
 
-    elif kind == 'crazy_name':
-        r1 = random.randint(1,15)
-        r2 = random.randint(15,30)
-        r3 = random.randint(1,30)
-        return  'S-{}-{}-{}'.format(r1, r2, r3)
 
-    elif kind == 'mac_address_no_colon':
-        return generate_random_hexdecimal(12, True)
+def hex(length=0):
+    return generate_random_hexdecimal(length, True)
 
-    elif kind == 'ip':
-        r1 = random.randint(1,255)
-        return '10.20.30.{}'.format(r1)
 
-    elif kind == 'guid':
-        r1 = generate_random_hexdecimal(8, False)
-        r2 = generate_random_hexdecimal(4, False)
-        r3 = generate_random_hexdecimal(4, False)
-        r4 = generate_random_hexdecimal(4, False)
-        r5 = generate_random_hexdecimal(12, False)
-        return '{}-{}-{}-{}-{}'.format(r1, r2, r3, r4, r5)
+def crazy_name(length=0):
+    r1 = random.randint(1,15)
+    r2 = random.randint(15,30)
+    r3 = random.randint(1,30)
+    return  'S-{}-{}-{}'.format(r1, r2, r3)
 
-    elif kind == 'machine_name':
-        letters = ['A', 'B', 'C', 'X', 'Y', 'Z']
-        r1 = ''.join(random.choice(letters) for i in range(4))
-        r2 = random.randint(100000,999999)
-        r3 = ''.join(random.choice(letters) for i in range(3))
-        return '{}{}-{}'.format(r1, r2, r3)
 
-    elif kind == 'external_id':
-        r1 = random.randint(100000,999999)
-        return str(r1)
+def machine_name(length=0):
+    letters = ['A', 'B', 'C', 'X', 'Y', 'Z']
+    r1 = ''.join(random.choice(letters) for i in range(4))
+    r2 = random.randint(100000,999999)
+    r3 = ''.join(random.choice(letters) for i in range(3))
+    return '{}{}-{}'.format(r1, r2, r3)
 
-    elif kind == 'dt_machine_name':
-        r1 = random.randint(1000,9999)
-        r2 = random.randint(10,99)
-        return 'dtools-dt-{}-{}'.format(r1, r2)
 
-    elif kind == 'shost':
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', '1', '2', '3', '4']
-        r1 = ''.join(random.choice(letters) for i in range(4))
-        return 'www.{}.com'.format(r1)
+def ipv4(length=0):
+    r1 = random.randint(1,255)
+    return '10.20.30.{}'.format(r1)
 
-    elif kind == 'user_name_with_domain':
-        domains = ['COMP', 'NT DOM']
-        r1 = ''.join(random.choice(domains) for i in range(1))
-        letters = ['A', 'E', 'I', 'O', 'U', 'K', 'L', 'M', 'X', 'Y', 'Z']
-        r2 = ''.join(random.choice(letters) for i in range(8))
-        return '{}\\\\\\\\{}'.format(r1, r2)
 
-    elif kind == 'user_name_2l_6n':
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'x', 'y', 'z']
-        r1 = ''.join(random.choice(letters) for i in range(2))
-        r2 = random.randint(100000,999999)
-        return '{}{}'.format(r1, r2)
+def guid(length=0):
+    r1 = generate_random_hexdecimal(8, False)
+    r2 = generate_random_hexdecimal(4, False)
+    r3 = generate_random_hexdecimal(4, False)
+    r4 = generate_random_hexdecimal(4, False)
+    r5 = generate_random_hexdecimal(12, False)
+    return '{}-{}-{}-{}-{}'.format(r1, r2, r3, r4, r5)
 
-    elif kind == 'user_name_upper':
-        letters = ['A', 'E', 'I', 'O', 'U', 'K', 'L', 'M', 'X', 'Y', 'Z']
-        r1 = ''.join(random.choice(letters) for i in range(8))
-        return '{}'.format(r1)
+
+def shost(length=0):
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', '1', '2', '3', '4']
+    r1 = ''.join(random.choice(letters) for i in range(4))
+    return 'www.{}.com'.format(r1)
+
+
+def external_id(length=0):
+    r1 = random.randint(100000,999999)
+    return str(r1)
+
+
+def dt_machine_name(length=0):
+    r1 = random.randint(1000,9999)
+    r2 = random.randint(10,99)
+    return 'dtools-dt-{}-{}'.format(r1, r2)
 
 
 if __name__ == "__main__":
